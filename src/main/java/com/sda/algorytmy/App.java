698038a0c265e40c4aba6b7043d489dd23162862
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-25 11:55:03 +0100

    Lista Imion z App

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,49 @@
+package com.sda.algorytmy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        Zadanie.zad1();
+//        Box<Integer> box1 = new Box(12);
+//       Box<Float> box2 = new Box(12.32);
+  /*      List<Integer>integerList=new ArrayList<>();
+
+        new Integer(231);
+        integerList.add(21);
+        integerList.add(211);
+        integerList.add(212);
+        integerList.add(213);
+        integerList.add(214);
+        integerList.add(215);
+        integerList.add(216);
+        integerList.add(217);
+        integerList.add(218);
+        integerList.add(219);
+        integerList.add(2110);
+        integerList.add(2111);
+        integerList.add(2112);
+        integerList.add(2113);
+        integerList.add(2114);
+        integerList.add(2115);
+        integerList.add(2116);
+        integerList.add(2117);
+
+        for(Integer element: integerList){
+            System.out.println(element);
+        }
+
+        for(int i=0; i<integerList.size();i++){
+            System.out.println(integerList.get(i));
+        }
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Zosia");
+        stringList.add("Przemek");
+*/
+    }
+}
